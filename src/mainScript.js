22681cf4bfc1dd7137c5057ffdd43ebf7e59afab
jhnemogap <jhnemogap@gmail.com>
2022-02-02 03:59:35 -0500

    feat: html structure of user card, and processing user data

--- src/mainScript.js
+++ src/mainScript.js
@@ -1,12 +1,20 @@
 const USERNAME_DEFAULT = "octocat";
+const TEXT_NOT_AVAILABLE = "Not Available";
+const TEXT_BIOGRAPHY_DEFAULT = "This profile has no bio";
+
 const DARK_THEME_NAME = "dark";
 const LIGHT_THEME_NAME = "light";
 const PREFERS_COLOR_SCHEME_DARK = "(prefers-color-scheme: dark)";
+
 
 new Vue({
   el: "#app",
 
   data: {
+    constants: {
+      TEXT_BIOGRAPHY_DEFAULT,
+      TEXT_NOT_AVAILABLE,
+    },
     colorTheme: {
       active: LIGHT_THEME_NAME,
       next: DARK_THEME_NAME,
@@ -33,6 +41,27 @@ new Vue({
         this.isSearchError = true;
       }
     },
+  },
+  computed: {
+    user: function() {
+      if (!this.searchResult) return null;
+      const data = this.searchResult;
+      const joinedA = new Date(data["created_at"]).toDateString().slice(4).split(" ");
+      return {
+        avatar: data["avatar_url"],
+        username: data["login"],
+        name: data["name"] || `${data["login"]}@`,
+        joined: `Joined ${[joinedA[1], joinedA[0], joinedA[2]].join(" ")}`,
+        biography: data["bio"],
+        repos: data["public_repos"],
+        followers: data["followers"],
+        following: data["following"],
+        location: data["location"],
+        website: data["blog"],
+        twitter: data["twitter_username"],
+        company: data["company"],
+      };
+    }
   },
 
   created: function() {
